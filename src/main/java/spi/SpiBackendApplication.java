64ad17fe5d774fa65959c61bb7dae7d1a15ccc7f
AmineBkUBO <aminejustagamer@gmail.com>
2026-01-22 15:49:49 +0100

    Initial Commit

---
+++ src/main/java/spi/SpiBackendApplication.java
@@ -0,0 +1,13 @@
+package spi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spi/SpiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package spi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
